58a540bf13ed2e0043021e3ff8e15f1b4c06c849
kesavan <arunprakash7871@gmail.com>
2023-02-15 02:20:52 -0500

    add some comments in javascript

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const isisland=false;
+//To asign the boolean data types 
 const country="india";
+//To Check the string
 const population=1.4 ;
+//number data types
 const language=
+//undefined data types
+
+
+
+
+//to check what the type we use typeof
 console.log(typeof isisland);
 console.log(typeof country);
 console.log(typeof population);
