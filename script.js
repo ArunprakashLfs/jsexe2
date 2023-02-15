42c4a1bcd67758c68395cd97c73aaa4dc0e5827a
kesavan <arunprakash7871@gmail.com>
2023-02-15 02:17:59 -0500

    sample js file to learning purpose

---
+++ script.js
@@ -0,0 +1,8 @@
+const isisland=false;
+const country="india";
+const population=1.4 ;
+const language=
+console.log(typeof isisland);
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof language);
