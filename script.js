e19a9ab741a4797ab6e76fc6ee647ee5fe2a4d2f
kesavan <arunprakash7871@gmail.com>
2023-02-17 02:30:14 -0500

    type coertion

--- script.js
+++ script.js
@@ -35,21 +35,21 @@ console.log(description);*/
 //inputs how you are getting
 //output form
 //2.Divide the sollution
-const markweight=82;
+/*const markweight=82;
 const markheight=1.74;
 
 const johnweight=96;
 const johnheight=1.92;
 /*BMI = mass / height ** 2 = mass / (height * height) (mass in kg
     and height in meter)*/
-markbmi=markweight/markheight**2;
+/*Smarkbmi=markweight/markheight**2;
 johnbmi=johnweight/ (johnheight * johnheight);
 console.log('MARK BMI' + '' +markbmi);
 console.log('JOHN BMI' + '' + johnbmi);
-console.log(markhigherbmi=(markbmi>johnbmi));
+console.log(markhigherbmi=(markbmi>johnbmi));*/
 
 //template literals using=`$()`
-const descriptionone=`${country} is in ${continent}, and its
+/*const descriptionone=`${country} is in ${continent}, and its
 ${population} million people speak ${language}`;
 console.log(descriptionone);
 
@@ -59,14 +59,46 @@ if(population>33){
 }
 else{
     console.log(`${country}population is below average`)
-}
+}*/
 //if else for bmi
-if(markbmi>johnbmi){
-    console.log(`Marks Bmi(${markbmi}) higher than johns( ${johnbmi} )`);
+//.tofixed(1) for one decimal
+/*if(markbmi>johnbmi){
+    console.log(`Marks Bmi(${markbmi.toFixed(2)}) higher than johns( ${johnbmi.toFixed(2)} )`);
 }
 else if (johnbmi>markbmi){
-    console.log(`JOHNs Bmi(${johnbmi}) higher then Markbmi(${markbmi})`);
+    console.log(`JOHNs Bmi(${johnbmi.toFixed(2)}) higher then Markbmi(${markbmi.toFixed(2)})`);
 }
 else{
     console.log("BOth equal bmi")
+}*/
+//type conversion and Coercion
+
+console.log('9'-'5');
+// This is based on operator apply and performs 
+// it is string value based on operator(-) it converts into numbers and performs
+console.log('19' - '13' + '17');
+// in first performs based on operator(-)it apply numer approch
+//in addition it performs string approch
+console.log('19' - '13' + 17);
+// in first performs based on operator(-)it apply nume
+//in add two number performs addtion
+console.log('123' < 57);
+
+//based on operator(<) it applies number approch
+console.log(5 + 6 + '4' + 9 - 4 - 2);
+// act number of add and string concordination after subtraction applied
+
+
+//TOPIC:-  Equality Operators
+const numNeighbours = prompt('How many neighbour countries does your country have?',);
+    // LATER : This helps us prevent bugs
+const numNeighbours  = Number( prompt('How many neighbour countries does your country have'),);
+if (numNeighbours===1){
+
+}
+else if (numNeighbours>1){
+    console.log("More than one border");
+}
+else{
+    console.log("No Borders")
 }
